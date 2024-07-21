b229b94a43236834a1ae551e258d7878f07e6121
Juha Halmu <jhalmu@users.noreply.github.com>
2024-07-21 00:34:24 +0000

    Working test creds for test.file

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :example, Example.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "codespace",
+  password: "password",
   hostname: "localhost",
   database: "example_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
